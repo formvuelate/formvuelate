cbc82e5c14b650c5eef17eb7efcfa4ad1adbb850
Marina Mosti <marina@mosti.com.mx>
2019-11-13 13:53:01 -0500

    Adds composition api plugin to docs

--- docs/.vuepress/enhanceApp.js
+++ docs/.vuepress/enhanceApp.js
@@ -1,9 +1,13 @@
+import CompositionAPI from '@vue/composition-api'
+
 import SchemaForm from '../../src/SchemaForm.vue'
 import SchemaWizard from '../../src/SchemaWizard.vue'
 
 export default ({
   Vue
 }) => {
+  Vue.use(CompositionAPI)
+
   Vue.component('SchemaForm', SchemaForm)
   Vue.component('SchemaWizard', SchemaWizard)
 }
