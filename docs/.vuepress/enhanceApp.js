1f13a4c66f2220c6fc49aae77e2c8f20d5a5f663
Marina Mosti <marina@mosti.com.mx>
2020-05-03 10:03:19 -0500

    Delete docs folder

--- docs/.vuepress/enhanceApp.js
+++
@@ -1,9 +0,0 @@
-import SchemaForm from '../../src/SchemaForm.vue'
-import SchemaWizard from '../../src/SchemaWizard.vue'
-
-export default ({
-  Vue
-}) => {
-  Vue.component('SchemaForm', SchemaForm)
-  Vue.component('SchemaWizard', SchemaWizard)
-}
