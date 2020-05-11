4c08669d900fdb9466c45539ea804f04a70756c3
Marina Mosti <marina@mosti.com.mx>
2020-05-11 10:03:53 -0500

    Remove hardcoded button type

--- .vitepress/docs/components/form-elements/BaseButton.vue
+++ .vitepress/docs/components/form-elements/BaseButton.vue
@@ -1,12 +1,13 @@
 <template>
-  <button v-on="$listeners" type="button"><slot/></button>
+  <button v-on="$listeners"><slot/></button>
 </template>
 
 <style lang="stylus" scoped>
-button
+button {
   color: #fff;
   background: #3eaf7c;
   padding: 12px 24px;
   font-size: 14px;
-  border: none
+  border: none;
+}
 </style>
