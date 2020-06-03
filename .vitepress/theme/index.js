c6351dc05976fb033f14c5ed58d16f8f2c18750c
Marina Mosti <marina@mosti.com.mx>
2020-06-03 11:05:45 -0500

    Update to vite .3

--- .vitepress/theme/index.js
+++ .vitepress/theme/index.js
@@ -1,25 +1,26 @@
-import Layout from './Layout.vue'
 import SplitTab from '/.vitepress/docs/components/SplitTab.vue'
 import FormText from '/.vitepress/docs/components/form-elements/FormText.vue'
 import FormSelect from '/.vitepress/docs/components/form-elements/FormSelect.vue'
 import FormCheckbox from '/.vitepress/docs/components/form-elements/FormCheckbox.vue'
 import BaseButton from '/.vitepress/docs/components/form-elements/BaseButton.vue'
 import SchemaForm from './../../src/SchemaForm.vue'
-import SidebarLinks from './components/SidebarLinks.vue'
-import SidebarLink from './components/SidebarLink.vue'
 import SchemaPlayground from '/.vitepress/docs/components/SchemaPlayground.vue'
 
 import ExampleVModel from '/.vitepress/docs/components/ExampleVModel.vue'
 import ArrayExample from '/.vitepress/docs/components/ArrayExample.vue'
 import Formception from '/.vitepress/docs/components/Formception.vue'
 
-export default {
+import './styles/vars.css';
+import './styles/layout.css';
+import './styles/code.css';
+import './styles/custom-blocks.css';
+import Layout from './Layout.vue';
+import NotFound from './NotFound.vue';
+const theme = {
   Layout,
-  NotFound: () => '404 ;<',
+  NotFound,
   enhanceApp ({ app, router, siteData }) {
     app.provide('router', router)
-    app.component('SidebarLinks', SidebarLinks)
-    app.component('SidebarLink', SidebarLink)
 
     app.component('FormText', FormText)
     app.component('FormSelect', FormSelect)
@@ -34,4 +35,5 @@ export default {
     app.component('ArrayExample', ArrayExample)
     app.component('Formception', Formception)
   }
-}
+};
+export default theme;
