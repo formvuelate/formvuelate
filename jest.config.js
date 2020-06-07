13fd0eb4dee70630175ed411042d80e855a7ff59
Marina Mosti <marina@mosti.com.mx>
2020-06-07 10:25:10 -0500

    Set up jest and vtu

--- jest.config.js
+++ jest.config.js
@@ -5,22 +5,29 @@ module.exports = {
     'json',
     'vue'
   ],
+
   transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub',
     '^.+\\.jsx?$': 'babel-jest'
   },
+
   transformIgnorePatterns: [
     '/node_modules/'
   ],
+
   moduleNameMapper: {
     '^@/(.*)$': '<rootDir>/src/$1'
   },
+
   snapshotSerializers: [
     'jest-serializer-vue'
   ],
+
   testMatch: [
     '**/tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx)'
   ],
-  testURL: 'http://localhost/'
+
+  testURL: 'http://localhost/',
+  preset: '@vue/cli-plugin-unit-jest'
 }
