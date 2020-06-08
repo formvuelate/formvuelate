1d5b365e37c2ad2e792a8dc77fc2eda0dcad7d39
Marina Mosti <marina@mosti.com.mx>
2020-06-08 07:24:07 -0500

    Add github link to docs

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -1,4 +1,13 @@
 module.exports = {
   title: 'FormVueLatte 2.0',
-  description: 'Documentation for FormVueLatte 2.0'
+  description: 'Documentation for FormVueLatte 2.0',
+  themeConfig: {
+    nav: [
+      {
+        text: 'GitHub',
+        link: 'https://github.com/vuelidate/formvuelatte',
+        target: '_blank'
+      }
+    ]
+  }
 }
--- .vitepress/theme/components/NavBar.vue
+++ .vitepress/theme/components/NavBar.vue
@@ -21,7 +21,10 @@
       :target="target"
       :rel="rel"
       :aria-label="ariaLabel"
-      >{{ text }}</a
+      >
+        {{ text }}
+        <svg v-if="target === '_blank'" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" x="0px" y="0px" viewBox="0 0 100 100" width="15" height="15" class="icon outbound"><path fill="currentColor" d="M18.8,85.1h56l0,0c2.2,0,4-1.8,4-4v-32h-8v28h-48v-48h28v-8h-32l0,0c-2.2,0-4,1.8-4,4v56C14.8,83.3,16.6,85.1,18.8,85.1z"></path> <polygon fill="currentColor" points="45.7,48.7 51.3,54.3 77.2,28.5 77.2,37.2 85.2,37.2 85.2,14.9 62.8,14.9 62.8,22.9 71.5,22.9"></polygon></svg>
+      </a
     >
   </nav>
 </template>
@@ -49,9 +52,15 @@
   color: var(--text-color);
   margin-left: 1.5rem;
   font-weight: 600;
-  display: inline-block;
+  display: inline-flex;
+  align-items: center;
   height: 1.75rem;
   line-height: 1.75rem;
+}
+
+.nav-link svg {
+  margin-left: 0.5rem;
+  color: grey;
 }
 
 .nav-link:hover,
