404f90ac4abcef0c205ad7cc6bd572bb0fa2aa12
dheimoz <382153+dheimoz@users.noreply.github.com>
2020-11-30 13:11:47 -0600

    feat:schemaRowClasses

--- src/SchemaForm.vue
+++ src/SchemaForm.vue
@@ -1,11 +1,15 @@
 <template>
-  <component :is="!hasParentSchema ? 'form' : 'div'" v-bind="formBinds">
-    <slot v-if="!hasParentSchema" name="beforeForm" />
+  <component
+    :is="!hasParentSchema ? 'form' : 'div'"
+    v-bind="formBinds"
+  >
+    <slot
+      v-if="!hasParentSchema"
+      name="beforeForm"
+    />
 
     <div
-      :class="[
-        additionalClassSchema === '' ? 'schema-row' : additionalClassSchema,
-      ]"
+      :class="['schema-row', schemaRowClasses]"
       v-for="(fields, index) in parsedSchema"
       :key="index"
     >
@@ -21,117 +25,120 @@
       />
     </div>
 
-    <slot v-if="!hasParentSchema" name="afterForm" />
+    <slot
+      v-if="!hasParentSchema"
+      name="afterForm"
+    />
   </component>
 </template>
 
 <script>
-import useParsedSchema from "./features/ParsedSchema";
-import { computed, watch, provide, inject } from "vue";
+import useParsedSchema from './features/ParsedSchema'
+import { computed, watch, provide, inject } from 'vue'
 
 export default {
   props: {
     schema: {
       type: [Object, Array],
       required: true,
-      validator(schema) {
-        if (!Array.isArray(schema)) return true;
+      validator (schema) {
+        if (!Array.isArray(schema)) return true
 
         return (
           schema.filter(
             (field) => !Array.isArray(field) && !field.model && !field.schema
           ).length === 0
-        );
-      },
+        )
+      }
     },
     modelValue: {
       type: Object,
-      required: true,
+      required: true
     },
     sharedConfig: {
       type: Object,
-      default: () => ({}),
+      default: () => ({})
     },
     preventModelCleanupOnSchemaChange: {
       type: Boolean,
-      default: false,
+      default: false
     },
-    additionalClassSchema: {
-      type: String,
-      default: "",
-    },
+    schemaRowClasses: {
+      type: [String, Object, Array],
+      default: null
+    }
   },
-  emits: ["submit", "update:modelValue"],
-  setup(props, { emit }) {
-    const hasParentSchema = inject("parentSchemaExists", false);
+  emits: ['submit', 'update:modelValue'],
+  setup (props, { emit }) {
+    const hasParentSchema = inject('parentSchemaExists', false)
     if (!hasParentSchema) {
-      provide("parentSchemaExists", true);
+      provide('parentSchemaExists', true)
     }
 
-    const { parsedSchema } = useParsedSchema(props);
+    const { parsedSchema } = useParsedSchema(props)
 
     const cleanupModelChanges = (schema, oldSchema) => {
-      if (props.preventModelCleanupOnSchemaChange) return;
+      if (props.preventModelCleanupOnSchemaChange) return
 
       const reducer = (acc, val) => {
-        return acc.concat(val.map((i) => i.model));
-      };
-
-      const newKeys = schema.reduce(reducer, []);
-      const oldKeys = oldSchema.reduce(reducer, []);
-
-      const diff = oldKeys.filter((i) => !newKeys.includes(i));
-      if (!diff.length) return;
-
-      const val = { ...props.modelValue };
-
-      for (const key of diff) {
-        delete val[key];
+        return acc.concat(val.map((i) => i.model))
       }
 
-      emit("update:modelValue", val);
-    };
+      const newKeys = schema.reduce(reducer, [])
+      const oldKeys = oldSchema.reduce(reducer, [])
 
-    watch(parsedSchema, cleanupModelChanges);
+      const diff = oldKeys.filter((i) => !newKeys.includes(i))
+      if (!diff.length) return
+
+      const val = { ...props.modelValue }
+
+      for (const key of diff) {
+        delete val[key]
+      }
+
+      emit('update:modelValue', val)
+    }
+
+    watch(parsedSchema, cleanupModelChanges)
 
     const update = (property, value) => {
-      emit("update:modelValue", {
+      emit('update:modelValue', {
         ...props.modelValue,
-        [property]: value,
-      });
-    };
+        [property]: value
+      })
+    }
 
     const updateBatch = (property, values) => {
-      emit("update:modelValue", {
+      emit('update:modelValue', {
         ...props.modelValue,
-        ...values,
-      });
-    };
+        ...values
+      })
+    }
 
     const binds = (field) => {
       return field.schema
         ? { schema: field.schema }
-        : { ...props.sharedConfig, ...field };
-    };
+        : { ...props.sharedConfig, ...field }
+    }
 
     const val = (field) => {
       if (field.schema && !props.modelValue[field.model]) {
-        return {};
+        return {}
       }
 
-      return props.modelValue[field.model];
-    };
+      return props.modelValue[field.model]
+    }
 
     const formBinds = computed(() => {
-      if (hasParentSchema) return {};
+      if (hasParentSchema) return {}
 
       return {
         onSubmit: (event) => {
-          event.preventDefault();
-          emit("submit", event);
-        },
-      };
-    });
+          event.preventDefault()
+          emit('submit', event)
+        }
+      }
+    })
 
     return {
       parsedSchema,
@@ -140,10 +147,10 @@ export default {
       update,
       updateBatch,
       hasParentSchema,
-      formBinds,
-    };
-  },
-};
+      formBinds
+    }
+  }
+}
 </script>
 
 <style>
