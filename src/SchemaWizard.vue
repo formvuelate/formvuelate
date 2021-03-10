4e33b66832f053e70fda0a3520eeac8265d49b1e
Marina Mosti <marina@mosti.com.mx>
2021-03-10 14:06:35 +0100

    update: SchemaWizard to use new useSchemaForm composable

--- src/SchemaWizard.vue
+++ src/SchemaWizard.vue
@@ -4,8 +4,6 @@
 
     <SchemaForm
       :schema="currentSchema"
-      :modelValue="modelValue[step] || {}"
-      @update:modelValue="update"
     />
 
     <slot name="afterForm" />
@@ -27,28 +25,17 @@ export default {
       type: Number,
       required: true,
       default: 0
-    },
-    modelValue: {
-      type: Array,
-      required: true
     }
   },
-  emits: ['submit', 'update:modelValue'],
-  setup (props, context) {
+  emits: ['submit'],
+  setup (props) {
     provide('parentSchemaExists', true)
 
     const currentSchema = computed(() => {
       return props.schema[props.step]
     })
 
-    const update = data => {
-      const value = [...props.modelValue]
-      value[props.step] = data
-
-      context.emit('update:modelValue', value)
-    }
-
-    return { currentSchema, update }
+    return { currentSchema }
   }
 }
 </script>
