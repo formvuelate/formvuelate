8814e8d7c7adb97c4b52674a76450802d967f5dc
Marina Mosti <marina@mosti.com.mx>
2021-03-21 15:40:28 +0100

    feat: add comments for clarity

--- src/features/InjectedSchema.js
+++ src/features/InjectedSchema.js
@@ -5,13 +5,18 @@ export default function useInjectedSchema (props) {
   const { schema } = toRefs(props)
 
   let injectedSchema = inject(INJECTED_SCHEMA, false)
-
   if (!injectedSchema) {
+    // Only the top level schema form should inject the schema
+    // That way we dont have to worry about injecting the prop down into
+    // sub schemas
     provide(INJECTED_SCHEMA, schema)
     injectedSchema = schema
   }
 
   if (props.nestedSchemaModel) {
+    // If the nestedSchemaModel prop is set it means this
+    // component is a subschema, and we need to inform descendants
+    // of the "path" for the model. ex. "info.family.parents"
     const path = inject(SCHEMA_MODEL_PATH, '')
     const currentPath = path ? `${path}.${props.nestedSchemaModel}` : props.nestedSchemaModel
 
--- src/features/ParsedSchema.js
+++ src/features/ParsedSchema.js
@@ -1,10 +1,21 @@
 import { computed, unref } from 'vue'
 import useUniqueID from './UniqueID'
 
+/**
+ * Find the elements in the row that have a schema property
+ * @param {Array} row
+ * @returns
+ */
 const findSchemaElementsInRow = (row) => {
   return row.filter(el => el.schema)
 }
 
+/**
+ * Find the elements in the top level row of a schema
+ * that are considered "schema" elements, aka. they have a schema prop
+ * @param {Array} normalizedSchema
+ * @returns
+ */
 const findSchemaElements = (normalizedSchema) => {
   for (const row of normalizedSchema) {
     const elements = findSchemaElementsInRow(row)
@@ -14,6 +25,13 @@ const findSchemaElements = (normalizedSchema) => {
   return []
 }
 
+/**
+ * Traverse a schema recursively and find the schema element
+ * that matches the given model
+ * @param {String} model
+ * @param {Array} normalizedSchema
+ * @returns
+ */
 const findElementInSchema = (model, normalizedSchema) => {
   const schemaElements = findSchemaElements(normalizedSchema)
   const isCorrectElement = el => el?.model === model
@@ -33,6 +51,11 @@ const findElementInSchema = (model, normalizedSchema) => {
   return null
 }
 
+/**
+ * Parse a user given schema into FVL internal format
+ * @param {Array|Object} schema
+ * @returns
+ */
 const normalizeSchema = (schema) => {
   const arraySchema = Array.isArray(schema)
     ? schema
@@ -54,6 +77,12 @@ export default function useParsedSchema (refSchema, model) {
     let normalizedSchema = normalizeSchema(schema)
 
     if (model) {
+      /**
+       * If the model is provided, it means a SchemaForm is trying to find
+       * a subschema in the main schema that corresponds to its "model" in the
+       * use provided schema. We dig into the sub schemas to find it and normalize it
+       * before setting it as the returned parsed schema
+       */
       const element = findElementInSchema(model, normalizedSchema)
       if (element) {
         normalizedSchema = normalizeSchema(element.schema)
