ce8f69362c4cfcb7935f61f301789ccb9f59dfc8
Abdelrahman Awad <logaretm1@gmail.com>
2021-08-01 20:52:11 +0200

    chore: remove indivudal configs

--- packages/plugin-lookup/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  globals: {},
-  testEnvironment: 'jsdom',
-  transform: {
-    "^.+\\.vue$": "vue-jest",
-    "^.+\\js$": "babel-jest"
-  },
-  moduleFileExtensions: ['vue', 'js', 'json', 'jsx', 'ts', 'tsx', 'node']
-}
